303eca4709e3ae3071b7a7eb7cfcdb65bd291b5d
asx-dev <asxdev18@gmail.com>
2025-06-09 12:43:30 +0100

    Completed Dashboard

--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -11,6 +11,11 @@ import Link from "next/link";
 import { Button } from "@/app/ui/button";
 import { updateInvoice, State } from "@/app/lib/actions"; // Assuming you have an updateInvoice function
 import { useActionState } from "react";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Edit Invoice",
+};
 
 export default function EditInvoiceForm({
   invoice,
